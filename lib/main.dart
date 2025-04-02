dd61936d066a5f4ec73e21a53250a055655544c6
Alis <bertha.ortega@udgvirtual.udg.mx>
2025-04-02 12:18:21 -0600

    corrección de error en texto

--- lib/main.dart
+++ lib/main.dart
@@ -12,40 +12,39 @@ class MyApp extends StatelessWidget {
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(title: const Text(appTitle)),
-        body: Stack(
-          children: [
-            // Imagen de fondo
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    'https://images.pexels.com/photos/3709369/pexels-photo-3709369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-                  ),
-                  fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child : Column (
+            children: [
+              ImageSection(
+                image: NetworkImage(
+                  'https://images.pexels.com/photos/3709369/pexels-photo-3709369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 ),
               ),
+            const TitleSection(name: 'Catálogo de películas', location: 'Animadas'),
+            const TitleSection(
+              name: 'Descripción',
+              location: 'En esta aplicación podrás ver una lista de películas animadas.',
             ),
-            // Contenido centrado
-            SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center, // Centrado vertical
-                crossAxisAlignment: CrossAxisAlignment.center, // Centrado horizontal
-                children: [
-                  const SizedBox(height: 16), // Espaciado inicial
-                  const TitleSection(
-                    name: 'Catálogo de películas',
-                    location: 'Animadas',
-                  ),
-                  const TitleSection(
-                    name: 'Descripción',
-                    location:
-                        'En esta aplicación podrás ver una lista de películas animadas.',
-                  ),
-                ],
-              ),
-            ),
-          ],
+           ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final ImageProvider image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Image(
+        image: image,
+        fit: BoxFit.cover,
       ),
     );
   }
@@ -61,29 +60,24 @@ class TitleSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32),
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center, // Centrado horizontal
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                name,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: Colors.black,
-                  textAlign: TextAlign.center, // Alinea el texto al centro
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold , fontSize: 24, color: Colors.black),
+                  ),
                 ),
-              ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
             ),
-            Text(
-              location,
-              textAlign: TextAlign.center, // Alinea el texto al centro
-              style: TextStyle(color: Colors.grey[500]),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -98,13 +92,10 @@ class TextSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32),
-      child: Center(
-        child: Text(
-          description,
-          textAlign: TextAlign.center, 
-          softWrap: true,
-        ),
-      ),
+      child: Text(
+        description,
+        textAlign: TextAlign.center, 
+        softWrap: true),
     );
   }
 }
